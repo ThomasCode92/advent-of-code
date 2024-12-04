5d26641c38b0b3df1b273309093513864292446a
Thomas <thomascode92@hotmail.com>
2024-12-04 20:17:44 +0100

    Solution Day 4 2024 - Part 2

--- 2024/day4.py
+++ 2024/day4.py
@@ -25,10 +25,17 @@ def find_word(x, y, index, dx, dy, grid, rows, cols, word):
     return find_word(x + dx, y + dy, index + 1, dx, dy, grid, rows, cols, word)
 
 
+# Check if the letter is 'A' and the surrounding letters form the pattern 'MAS'
+def matches_xmas_pattern(letter, x, y):
+    pattern = {"M", "S"}
+    if not letter == "A":
+        return False
+    return {data[x - 1][y - 1], data[x + 1][y + 1]} == pattern and {data[x - 1][y + 1], data[x + 1][y - 1]} == pattern
+
+
 def solve_puzzle_1(grid):
     word = "XMAS"
-    rows = len(grid)
-    cols = len(grid[0])
+    rows, cols = len(grid), len(grid[0])
 
     total_count = 0
     for x in range(rows):
@@ -40,6 +47,17 @@ def solve_puzzle_1(grid):
     return total_count
 
 
+def solve_puzzle_2(data):
+    rows, cols = len(data), len(data[0])
+
+    xmas_count = 0
+    for x in range(1, rows - 1):
+        for y in range(1, cols - 1):
+            if matches_xmas_pattern(data[x][y], x, y):
+                xmas_count += 1
+    return xmas_count
+
+
 if __name__ == "__main__":
     from sys import stdin
 
@@ -47,5 +65,7 @@ if __name__ == "__main__":
     data = parse_input(input)
 
     result_1 = solve_puzzle_1(data)
+    result_2 = solve_puzzle_2(data)
 
     print("result for puzzle 1:", result_1)
+    print("result for puzzle 2:", result_2)
