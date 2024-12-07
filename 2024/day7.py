4b8a7c62591bc6d6acb744cbdba0c661ae5390fe
Thomas <thomascode92@hotmail.com>
2024-12-07 14:28:01 +0100

    Solution Day 7 2024 - Part1

---
+++ 2024/day7.py
@@ -0,0 +1,53 @@
+from itertools import product
+
+
+def parse_input(input):
+    results, equations = [], []
+    for line in input.split("\n"):
+        result, numbers = line.split(":")
+        results.append(int(result))
+        equations.append([int(n) for n in numbers.split()])
+    return results, equations
+
+
+def evaluate_expression(numbers, operators):
+    """Evaluates an expression from numbers and operators (left-to-right)."""
+    result = numbers[0]
+    for i, op in enumerate(operators):
+        if op == "+":
+            result += numbers[i + 1]
+        elif op == "*":
+            result *= numbers[i + 1]
+    return result
+
+
+def solve_puzzle_1(test_values, calibrations):
+    """Finds which equations can be true and calculates the total calibration result."""
+    total_calibration = 0
+
+    for i, calibration in enumerate(calibrations):
+        test_value = test_values[i]
+        n = len(calibration) - 1  # Number of operator slots
+        possible = False
+
+        # Try all combinations of + and * operators
+        for operators in product("+*", repeat=n):
+            if evaluate_expression(calibration, operators) == test_value:
+                possible = True
+                break
+
+        if possible:
+            total_calibration += test_value
+
+    return total_calibration
+
+
+if __name__ == "__main__":
+    from sys import stdin
+
+    input = stdin.read().strip()
+    data = parse_input(input)
+
+    result_1 = solve_puzzle_1(*data)
+
+    print("result for puzzle 1:", result_1)
